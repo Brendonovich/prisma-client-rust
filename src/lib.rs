eda164345f64ab7e9b57f66f0d2f18ec8b67814d
Brendan Allan <brendonovich@outlook.com>
2022-04-15 07:34:18 +0800

    refactor: export cli as separate package

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ pub mod traits;
 
 use ::serde::{Deserialize, Serialize};
 pub use chrono;
-#[cfg(feature = "cli")]
-pub use cli;
 pub use datamodel;
 use datamodel::datamodel_connector::Diagnostics;
 pub use prisma_models;
