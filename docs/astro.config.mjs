c7b77d863d8e74622baa315ad4e00d3f84e32372
Brendan Allan <brendonovich@outlook.com>
2022-08-18 04:43:29 +0800

    tailwind + flexbox docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-  ],
-  site: `https://prisma.brendonovich.dev`,
+  integrations: [// Enable Preact to support Preact JSX components.
+  preact(), // Enable React for the Algolia search component.
+  react(), tailwind()],
+  site: `https://prisma.brendonovich.dev`
 });
