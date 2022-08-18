fbdb5d010abee27158999ef48542fa075ad54916
Brendan Allan <brendonovich@outlook.com>
2022-08-18 22:38:30 +0800

    manually apply tailwind base styles

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,7 +7,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [// Enable Preact to support Preact JSX components.
-  preact(), // Enable React for the Algolia search component.
-  react(), tailwind()],
+    preact(), // Enable React for the Algolia search component.
+    react(), tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    })],
   site: `https://prisma.brendonovich.dev`
 });
