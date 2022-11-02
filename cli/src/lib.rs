7570a46f0692e9e6abb785f158308f7f09efbee8
Brendan Allan <brendonovich@outlook.com>
2022-11-02 14:06:43 +0800

    move cli stuff out of sdk

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,14 +1,21 @@
+mod binaries;
 mod generator;
+mod prisma_cli;
 
+use prisma_client_rust_sdk::*;
 use std::env;
 
 use generator::PrismaClientRustGenerator;
-use prisma_client_rust_sdk::execute;
 
 pub fn run() {
     let args = env::args();
 
     let args = args.skip(1).collect::<Vec<_>>();
 
-    execute::<PrismaClientRustGenerator>(&args);
+    if std::env::var("PRISMA_GENERATOR_INVOCATION").is_err() {
+        prisma_cli::main(&args);
+        return;
+    }
+
+    PrismaClientRustGenerator::run();
 }
