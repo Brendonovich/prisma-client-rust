45cbccf2932f02a6a5219823f6f77cd579b9c741
Brendan Allan <brendonovich@outlook.com>
2023-03-24 17:27:32 +0800

    partial proc macro

--- cli/src/generator/models/partial.rs
+++ cli/src/generator/models/partial.rs
@@ -7,103 +7,25 @@ pub fn model_macro<'a>(model: &'a dml::Model, module_path: &TokenStream) -> Toke
 
     let model_module = quote!($crate::#module_path::#model_name_snake);
 
-    let field_type_arms = model.scalar_fields().map(|scalar_field| {
+    let struct_fields = model.scalar_fields().map(|scalar_field| {
+        let field_name_str = &scalar_field.name;
         let field_name_snake = snake_ident(&scalar_field.name);
         let field_type = scalar_field
             .field_type
             .to_tokens(quote!(crate::#module_path::), &scalar_field.arity);
 
+        let double_option_attrs = scalar_field.arity.is_optional().then(|| {
+            quote! {
+                #[serde(default, with = "::prisma_client_rust::serde::double_option")]
+            }
+        });
+
         quote! {
-            (@field_type; #field_name_snake) => { #field_type };
+            #[serde(rename = #field_name_str)]
+            #double_option_attrs
+            #field_name_snake: #field_type
         }
     });
-
-    let to_params_fn = {
-        quote! {
-            pub fn to_params(self) -> Vec<#model_module::SetParam> {
-                [
-                    $(self.$scalar_field.map(#model_module::$scalar_field::set)),+
-                ].into_iter().flatten().collect()
-            }
-        }
-    };
-
-    let deserialize_impl = {
-        let field_names_str = model.fields().map(|f| f.name());
-
-        quote! {
-            #[allow(warnings)]
-            enum Field {
-                $($scalar_field),+,
-            }
-
-            impl<'de> ::serde::Deserialize<'de> for Field {
-                fn deserialize<D>(deserializer: D) -> Result<Field, D::Error>
-                where
-                    D: ::serde::Deserializer<'de>,
-                {
-                    struct FieldVisitor;
-
-                    impl<'de> ::serde::de::Visitor<'de> for FieldVisitor {
-                        type Value = Field;
-
-                        fn expecting(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-                            formatter.write_str(&[
-                                $(#model_module::$scalar_field::NAME),+,
-                            ].into_iter().collect::<Vec<_>>().join(", "))
-                        }
-
-                        fn visit_str<E>(self, value: &str) -> Result<Field, E>
-                        where
-                            E: ::serde::de::Error,
-                        {
-                            match value {
-                                $(#model_module::$scalar_field::NAME => Ok(Field::$scalar_field)),*,
-                                _ => Err(::serde::de::Error::unknown_field(value, FIELDS)),
-                            }
-                        }
-                    }
-
-                    deserializer.deserialize_identifier(FieldVisitor)
-                }
-            }
-
-            struct DataVisitor;
-
-            impl<'de> ::serde::de::Visitor<'de> for DataVisitor {
-                type Value = $struct_name;
-
-                fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                    formatter.write_str(concat!("struct ", stringify!($struct_name)))
-                }
-
-                fn visit_map<V>(self, mut map: V) -> Result<$struct_name, V::Error>
-                where
-                    V: ::serde::de::MapAccess<'de>,
-                {
-                    $(let mut $scalar_field = None;)*
-
-                    while let Some(key) = map.next_key()? {
-                        match key {
-                            $(Field::$scalar_field => {
-                                if $scalar_field.is_some() {
-                                    return Err(::serde::de::Error::duplicate_field(
-                                        #model_module::$scalar_field::NAME
-                                    ));
-                                }
-                                $scalar_field = Some(map.next_value()?);
-                            })*
-                        }
-                    }
-
-                    Ok($struct_name { $($scalar_field),* })
-                }
-            }
-
-            const FIELDS: &'static [&'static str] = &[#(#field_names_str),*];
-            deserializer.deserialize_struct(stringify!($struct_name), FIELDS, DataVisitor)
-        }
-    };
 
     quote! {
         #[macro_export]
@@ -111,25 +33,14 @@ pub fn model_macro<'a>(model: &'a dml::Model, module_path: &TokenStream) -> Toke
             ($struct_name:ident {
                 $($scalar_field:ident)+
             }) => {
-                #[derive(PartialEq)]
-                pub struct $struct_name {
-                    $(pub $scalar_field: Option<#model_module::partial!(@field_type; $scalar_field)>),+
-                }
-
-                impl $struct_name {
-                    #to_params_fn
-                }
-
-                impl<'de> ::serde::Deserialize<'de> for $struct_name {
-                    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-                    where
-                        D: ::serde::Deserializer<'de>,
-                    {
-                        #deserialize_impl
+                ::prisma_client_rust::macros::partial! {
+                    #model_module
+                    struct $struct_name {
+                        #(#struct_fields),*
                     }
+                    [$($scalar_field),+]
                 }
             };
-            #(#field_type_arms)*
         }
 
         pub use #macro_name as partial;
