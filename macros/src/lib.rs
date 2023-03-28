7458c9a5b4368f7e0a96644fd11dd69a51848835
Brendan Allan <brendonovich@outlook.com>
2023-03-28 13:03:55 +0800

    separate partial macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,8 +1,4 @@
-use proc_macro2::Ident;
-use quote::quote;
-use syn::{
-    bracketed, parse::Parse, parse_macro_input, punctuated::Punctuated, ItemStruct, Path, Token,
-};
+mod partial;
 
 #[proc_macro]
 pub fn to_pascal_case(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -10,76 +6,7 @@ pub fn to_pascal_case(input: proc_macro::TokenStream) -> proc_macro::TokenStream
 
     proc_macro::TokenTree::Literal(proc_macro::Literal::string(&converted)).into()
 }
-
-struct PartialInput {
-    model_module: Path,
-    data: ItemStruct,
-    selection: Punctuated<Ident, Token![,]>,
-}
-
-impl Parse for PartialInput {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        Ok(Self {
-            model_module: input.parse()?,
-            data: input.parse()?,
-            selection: {
-                let content;
-                bracketed!(content in input);
-                Punctuated::<Ident, Token![,]>::parse_terminated(&content)?
-            },
-        })
-    }
-}
-
 #[proc_macro]
 pub fn partial(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let PartialInput {
-        model_module,
-        data,
-        selection,
-    } = parse_macro_input!(input as PartialInput);
-
-    let fields = data
-        .fields
-        .iter()
-        .filter(|f| selection.iter().any(|s| s == f.ident.as_ref().unwrap()))
-        .map(|field| {
-            let attrs = &field.attrs;
-            let ident = &field.ident;
-            let ty = &field.ty;
-
-            quote! {
-                #(#attrs)*
-                #ident: Option<#ty>
-            }
-        });
-
-    let specta_attrs = cfg!(feature = "specta").then(|| {
-        quote! {
-            #[derive(::prisma_client_rust::specta::Type)]
-            #[specta(crate = "prisma_client_rust::specta")]
-        }
-    });
-
-    let ident = &data.ident;
-
-    let selection = selection.iter().collect::<Vec<_>>();
-
-    quote!(
-        #[derive(serde::Deserialize)]
-        #specta_attrs
-        #[allow(unused)]
-        pub struct #ident {
-           #(#fields),*
-        }
-
-        impl #ident {
-            pub fn to_params(self) -> Vec<#model_module::SetParam> {
-                [
-                    #(self.#selection.map(#model_module::#selection::set)),*
-                ].into_iter().flatten().collect()
-            }
-        }
-    )
-    .into()
+    partial::proc_macro(input)
 }
