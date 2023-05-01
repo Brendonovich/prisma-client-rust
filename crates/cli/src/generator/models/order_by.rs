d1b4d919a8204b46538665ecbaf94cded6eda03e
Brendan Allan <brendonovich@outlook.com>
2023-05-02 04:41:22 +0800

    mongodb pending

--- crates/cli/src/generator/models/order_by.rs
+++ crates/cli/src/generator/models/order_by.rs
@@ -3,10 +3,9 @@ use std::collections::BTreeMap;
 use prisma_client_rust_sdk::prisma::{
     dmmf::TypeLocation,
     prisma_models::{
-        walkers::{ModelWalker, RefinedFieldWalker},
+        walkers::ModelWalker,
         FieldArity,
     },
-    psl::parser_database::ScalarFieldType,
 };
 
 use crate::generator::prelude::*;
@@ -24,58 +23,6 @@ pub fn fetch_builder_fn(model_name_snake: &Ident) -> TokenStream {
 
 pub fn model_data(model: ModelWalker, args: &GenerateArgs) -> SomethingThatNeedsFieldModules {
     let pcr = quote!(::prisma_client_rust);
-
-    let fields = model
-        .fields()
-        .flat_map(|field| {
-            let field_name_pascal = pascal_ident(field.name());
-
-            let field_fn = match field.refine() {
-                RefinedFieldWalker::Scalar(scalar_field) => {
-                    match scalar_field.scalar_field_type() {
-                        ScalarFieldType::BuiltInScalar(_) | ScalarFieldType::Enum(_) => {
-                            quote! {
-                            }
-                        }
-                        ScalarFieldType::CompositeType(id) => {
-                            let composite_type_snake = snake_ident(model.db.walk(id).name());
-
-                            if field.ast_field().arity.is_list() {
-                                return None;
-                            }
-
-                            let variant_type = quote!(Vec<#composite_type_snake::OrderByParam>);
-
-                            quote! {
-                                pub fn order(direction: #variant_type) -> OrderByParam {
-                                    OrderByParam::#field_name_pascal(direction)
-                                }
-                            }
-                        }
-                        ScalarFieldType::Unsupported(_) => return None,
-                    }
-                }
-                RefinedFieldWalker::Relation(relation_field) => {
-                    let relation_model_name_str = relation_field.related_model().name();
-                    let relation_model_name_snake = snake_ident(relation_model_name_str);
-
-                    let variant_type =
-                        quote!(#relation_model_name_snake::OrderByRelationAggregateParam);
-
-                    match relation_field.referential_arity() {
-                        FieldArity::List => quote! {
-                            pub fn order(direction: Vec<#variant_type>) -> super::OrderByWithRelationParam {
-                                super::OrderByWithRelationParam::#field_name_pascal(direction)
-                            }
-                        },
-                        _ => return None,
-                    }
-                }
-            };
-
-            Some((field.name().to_string(), field_fn))
-        })
-        .collect::<BTreeMap<_, _>>();
 
     let (order_by_relation_aggregate_param, aggregate_field_stuff) = args
         .dmmf
