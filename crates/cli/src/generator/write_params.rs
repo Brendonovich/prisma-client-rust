e38b9b48f81fa1788b7feaca567c26a0188b70d1
Brendan Allan <brendonovich@outlook.com>
2023-07-13 22:01:30 +0800

    put write_params in map before generating to avoid duplicates

--- crates/cli/src/generator/write_params.rs
+++ crates/cli/src/generator/write_params.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use super::prelude::*;
 
 pub fn enum_name(filter: &Filter) -> Ident {
@@ -5,54 +7,65 @@ pub fn enum_name(filter: &Filter) -> Ident {
 }
 
 pub fn generate_module(args: &GenerateArgs) -> TokenStream {
-    let write_params = args.write_params.iter().map(|write_param| {
-        let name = enum_name(write_param);
-
-        let (method_variants, method_matches): (Vec<_>, Vec<_>) = write_param
-            .fields
+    let write_params = {
+        let write_params_map = args
+            .write_params
             .iter()
-            .flat_map(|field| {
-                let typ = field.type_tokens(&quote!(super::super::));
-                let action = &field.name;
+            .map(|write_param| (&write_param.name, write_param))
+            .collect::<BTreeMap<_, _>>();
 
-                let prisma_value_converter = field.to_prisma_value(&format_ident!("value"));
+        write_params_map
+            .values()
+            .map(|write_param| {
+                let name = enum_name(write_param);
 
-                let method_name_pascal = pascal_ident(&field.name);
+                let (method_variants, method_matches): (Vec<_>, Vec<_>) = write_param
+                    .fields
+                    .iter()
+                    .flat_map(|field| {
+                        let typ = field.type_tokens(&quote!(super::super::));
+                        let action = &field.name;
 
-                // Set doesn't use 'set' as the action name.
-                let prisma_value = if action == "set" {
-                    prisma_value_converter
-                } else {
-                    quote! {
-                        ::prisma_client_rust::PrismaValue::Object(vec![(
-                            #action.to_string(),
-                            #prisma_value_converter
-                        )])
+                        let prisma_value_converter = field.to_prisma_value(&format_ident!("value"));
+
+                        let method_name_pascal = pascal_ident(&field.name);
+
+                        // Set doesn't use 'set' as the action name.
+                        let prisma_value = if action == "set" {
+                            prisma_value_converter
+                        } else {
+                            quote! {
+                                ::prisma_client_rust::PrismaValue::Object(vec![(
+                                    #action.to_string(),
+                                    #prisma_value_converter
+                                )])
+                            }
+                        };
+
+                        Some((
+                            quote!(#method_name_pascal(#typ)),
+                            quote!(Self::#method_name_pascal(value) => #prisma_value),
+                        ))
+                    })
+                    .unzip();
+
+                quote! {
+                    #[derive(Debug, Clone)]
+                    pub enum #name {
+                        #(#method_variants),*
                     }
-                };
 
-                Some((
-                    quote!(#method_name_pascal(#typ)),
-                    quote!(Self::#method_name_pascal(value) => #prisma_value),
-                ))
-            })
-            .unzip();
-
-        quote! {
-            #[derive(Debug, Clone)]
-            pub enum #name {
-                #(#method_variants),*
-            }
-
-            impl Into<::prisma_client_rust::PrismaValue> for #name {
-                fn into(self) -> ::prisma_client_rust::PrismaValue {
-                    match self {
-                        #(#method_matches),*
+                    impl Into<::prisma_client_rust::PrismaValue> for #name {
+                        fn into(self) -> ::prisma_client_rust::PrismaValue {
+                            match self {
+                                #(#method_matches),*
+                            }
+                        }
                     }
                 }
-            }
-        }
-    });
+            })
+            .collect::<Vec<_>>()
+    };
 
     quote! {
         pub mod write_params {
